74083e873c2269b3253720790b386f9fd621db4a
AnansiOmega <ignasbutautas7@gmail.com>
2020-10-21 10:04:15 -0500

    first commit

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,33 @@
+class NotesController < ApplicationController
+
+
+    def index
+        notes = Note.all
+        render json: notes
+    end
+
+    def create
+        note = Note.create(note_params)
+        render json: note
+    end
+
+    def destroy
+        note = Note.find(params[:id])
+        note.destroy
+    end
+
+    def update
+        note = Note.find(params[:id])
+        note.update(note_params)
+        render json: note
+    end
+
+
+
+
+    private
+
+    def note_params
+        params.require(:note).permit(:title,:body, :user_id)
+    end
+end
---
+++ app/serializers/note_serializer.rb
@@ -0,0 +1,6 @@
+class NoteSerializer < ActiveModel::Serializer
+  attributes :id, :title, :body
+
+  belongs_to :user
+  
+end
